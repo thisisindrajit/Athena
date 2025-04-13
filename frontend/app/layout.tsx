a0a566ead49f7ee1c331f20fe0176ccf4d6a192b
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-13 21:58:10 +0530

    added vercel speed insights and analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { APP_NAME, APP_DESCRIPTION } from "@/constants/common";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 const ibmPlexSans = IBM_Plex_Sans({
@@ -22,6 +25,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${ibmPlexSans.className} antialiased p-4 md:p-6`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

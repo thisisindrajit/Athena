728381ace89f35c3a5aa20b72fe8e0b352a31196
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-13 20:21:48 +0530

    added frontend code, added details in README.md and other changes

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { APP_NAME, APP_DESCRIPTION } from "@/constants/common";
+import "./globals.css";
+
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: APP_NAME,
+  description: APP_DESCRIPTION,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${ibmPlexSans.className} antialiased p-4 md:p-6`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="font-medium">Athena</div>;
+}

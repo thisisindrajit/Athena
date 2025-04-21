ec57ef148c8404d829bd4b59a2b8afa6ded9ee20
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-21 14:02:28 +0530

    small styling changes and removed loading for user

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -24,14 +24,14 @@ const Home = async () => {
   return (
     <>
       {/* Motto */}
-      <div className="h-48 md:h-42 w-full bg-dot-black/[0.2] dark:bg-dot-white/[0.2] relative flex items-center justify-center text-2xl/snug xs:text-3xl/snug lg:text-4xl/snug font-bold self-center text-center">
+      <div className="h-48 md:h-42 w-full bg-dot-black/[0.2] dark:bg-dot-white/[0.2] relative flex items-center justify-center text-2xl/snug xxs:text-3xl/snug lg:text-4xl/snug font-bold self-center text-center">
         {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]"></div>
-        <div className="hidden xs:block md:hidden">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_60%,black)]"></div>
+        <div className="hidden xxs:block md:hidden">
           <div>Where Knowledge is </div>
           <div className="text-primary">Crafted for you.</div>
         </div>
-        <div className="block xs:hidden md:block">
+        <div className="block xxs:hidden md:block">
           Where Knowledge is{" "}
           <span className="text-primary">Crafted for You.</span>
         </div>
--- frontend/app/user/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Loader from "@/components/common/Loader";
-
-const Loading = () => {
-  return <Loader />;
-};
-
-export default Loading;

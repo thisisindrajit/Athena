312f24897f5786509f7a568afec6ad8f568bc403
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-21 13:38:12 +0530

    fixed bug and testing out experimental stale times to cache pages

--- frontend/components/common/SnippetCard.tsx
+++ frontend/components/common/SnippetCard.tsx
@@ -1,4 +1,4 @@
-import { Separator } from "./ui/separator";
+import { Separator } from "@/components/ui/separator";
 
 const SnippetCard = () => {
   return (
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    staleTimes: {
+      dynamic: 0, // Dynamic pages will not be cached
+      static: 300, // Static pages will be cached for 5 minutes
+    },
+  },
 };
 
 export default nextConfig;

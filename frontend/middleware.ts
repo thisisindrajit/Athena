75c96550cf65b7b5666f20579d62b789a186919a
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-21 18:16:42 +0530

    checking safe area again

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-  const cookies = getSessionCookie(request);
-  if (!cookies) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   const cookies = getSessionCookie(request);
+//   if (!cookies) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/user/:path*"],
+//   matcher: ["/user/:path*"],
 };

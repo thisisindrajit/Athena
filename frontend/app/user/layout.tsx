ab0b1d47b0937d3828f81e4c3309deed3ff92a6e
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-21 19:21:52 +0530

    changed bottom bar from fixed to sticky

--- frontend/app/user/layout.tsx
+++ frontend/app/user/layout.tsx
@@ -69,23 +69,6 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         {/* Main content */}
         <div className="w-full flex flex-col gap-4">{children}</div>
       </div>
-      {/* Bottom bar (For size upto lg) */}
-      <div className="footer fixed lg:hidden left-0 p-1 px-4 bg-background flex items-start justify-between gap-2 w-full border-t text-sm text-muted-foreground dark:text-foreground pb-safe bottom-0 h-[calc(4.25rem+env(safe-area-inset-bottom))]">
-        {routes.map((route) => (
-          <Link href={route.href} key={route.href} className="w-full">
-            <div
-              className={`w-full p-3 rounded-md flex items-center justify-center gap-2 font-medium ${
-                route.active
-                  ? "text-primary bg-primary/10"
-                  : "transition-all cursor-pointer"
-              }`}
-            >
-              <route.icon className="h-4 w-4" />
-              <span className="hidden xs:block">{route.label}</span>
-            </div>
-          </Link>
-        ))}
-      </div>
     </>
   );
 };
--- frontend/components/common/CFooter.tsx
+++ frontend/components/common/CFooter.tsx
@@ -3,38 +3,77 @@
 import { APP_NAME } from "@/constants/common";
 import CDialogHolder from "../holders/DialogHolder";
 import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
+import { Bookmark, House, TrendingUp } from "lucide-react";
+import { useEffect } from "react";
+import Link from "next/link";
 
 const CFooter = () => {
   const pathname = usePathname();
-  const isUserRoute = pathname?.includes("user");
+  const routes = [
+    {
+      label: "Home",
+      icon: House,
+      href: "/user/dashboard",
+      active: pathname === "/user/dashboard",
+    },
+    {
+      label: "Trending",
+      icon: TrendingUp,
+      href: "/user/trending",
+      active: pathname === "/user/trending",
+    },
+    {
+      label: "Saved",
+      icon: Bookmark,
+      href: "/user/saved",
+      active: pathname === "/user/saved",
+    },
+  ];
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
-    <div
-      className={cn(
-        "bg-primary/5 text-primary m-auto w-full p-4 rounded-t-lg text-sm border-t border-primary/50 border-dashed flex items-center justify-center gap-2",
-        isUserRoute && "mb-[calc(4.25rem+env(safe-area-inset-bottom))] lg:mb-0"
-      )}
-    >
-      <CDialogHolder
-        trigger={
-          <div className="underline underline-offset-4 font-bold cursor-pointer">
-            What is {APP_NAME}?
+    <>
+      <div className="bg-primary/5 text-primary m-auto w-full p-4 rounded-t-lg text-sm border-t border-primary/50 border-dashed flex items-center justify-center gap-2">
+        <CDialogHolder
+          trigger={
+            <div className="underline underline-offset-4 font-bold cursor-pointer">
+              What is {APP_NAME}?
+            </div>
+          }
+          title={`What is ${APP_NAME}?`}
+        >
+          <div className="h-[100dvh] text-sm flex items-center justify-center">
+            This is the dialog
           </div>
-        }
-        title={`What is ${APP_NAME}?`}
-      >
-        <div className="h-[100dvh] text-sm flex items-center justify-center">
-          This is the dialog
+        </CDialogHolder>
+        <div className="hidden xsm:block">|</div>
+        <div className="hidden xsm:block">
+          Created by <span className="font-bold">Dhilip</span>
+          {" and "}
+          <span className="font-bold">Indrajit</span>
         </div>
-      </CDialogHolder>
-      <div className="hidden xsm:block">|</div>
-      <div className="hidden xsm:block">
-        Created by <span className="font-bold">Dhilip</span>
-        {" and "}
-        <span className="font-bold">Indrajit</span>
       </div>
-    </div>
+      {/* Bottom bar (For size upto lg) */}
+      <div className="sticky lg:hidden left-0 p-1.5 px-4 bg-background flex items-start justify-between gap-2 w-full border-t text-sm text-muted-foreground dark:text-foreground pb-safe bottom-safe h-14">
+        {routes.map((route) => (
+          <Link href={route.href} key={route.href} className="w-full">
+            <div
+              className={`w-full p-3 rounded-md flex items-center justify-center gap-2 font-medium ${
+                route.active
+                  ? "text-primary bg-primary/10"
+                  : "transition-all cursor-pointer"
+              }`}
+            >
+              <route.icon className="h-4 w-4" />
+              <span className="hidden xs:block">{route.label}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 

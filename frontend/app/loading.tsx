bd9d45e3fcb8e24d811138307d185e1ff1669bf7
Indrajit Kutuva <ikutuva@microsoft.com>
2025-04-29 15:13:05 +0530

    testing out footer in small screens

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,9 +1,9 @@
 import Loader from "@/components/common/Loader";
 
 const Loading = () => {
-  // The pt-32 class is to handle the pb-32 class in the main content container div in layout.tsx
-  // The pb-32 class is added so that the bottom bar does not hide the last part of the page
-  return <Loader className="pt-32 lg:pt-0 lg:min-h-[calc(100dvh-9.5rem)]" />;
+  // The pt-16 class is to handle the pb-16 class in the main content container div in layout.tsx
+  // The pb-16 class is added so that the bottom bar does not hide the footer
+  return <Loader className="pt-16 lg:pt-0 lg:min-h-[calc(100dvh-9.5rem)]" />;
 };
 
 export default Loading;

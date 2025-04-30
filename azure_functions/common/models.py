48a36bcaccf91760355eee0588cd30ab67e864e6
DhilipSanjay <53406309+DhilipSanjay@users.noreply.github.com>
2025-05-01 03:12:43 +0530

    remove load env

--- azure_functions/common/models.py
+++ azure_functions/common/models.py
@@ -1,9 +1,7 @@
 import os
 from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 from common.constants import AZURE_GPT_4O_MINI, AZURE_O3_MINI
-from dotenv import load_dotenv
 
-load_dotenv()
 gpt_4o_model_client = AzureOpenAIChatCompletionClient(
     azure_deployment=AZURE_GPT_4O_MINI,
     model=AZURE_GPT_4O_MINI,

3887545ca4238e7d4cae4a4bf24273cad397d868
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-30 13:44:00 +0530

    removed loading

--- frontend/app/course/[courseId]/module/[moduleId]/lesson/[lessonId]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Loader from "@/components/common/Loader";
-
-const LessonLoading = () => {
-    return <Loader loadingText="Loading lesson" />;
-}
- 
-export default LessonLoading;
--- frontend/app/course/[courseId]/module/[moduleId]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Loader from "@/components/common/Loader";
-
-const ModuleLoading = () => {
-    return <Loader loadingText="Loading module" />;
-}
- 
-export default ModuleLoading;

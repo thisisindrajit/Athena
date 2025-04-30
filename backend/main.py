aea7fdb447baa8d098abcd7a08535bc5d6234a86
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-04-30 13:12:38 +0530

    testing fast.api

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-import asyncio
-from swarm.team_swarm import run_team_swarm
-from selector.team_selector import run_team_selector
+# import asyncio
+# from swarm.team_swarm import run_team_swarm
+# from selector.team_selector import run_team_selector
 
-# asyncio.run(run_team_swarm())
-asyncio.run(run_team_selector())
+# # asyncio.run(run_team_swarm())
+# asyncio.run(run_team_selector())
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
